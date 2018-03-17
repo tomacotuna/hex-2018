e900fa6b8bdb679a4ec3aae0d1085a7a4610d902
shakeelsubratty <shakeel.subratty@gmail.com>
2018-03-17 15:04:20 +0100

    Fix user queries by id

--- backend/user/User.js
+++ backend/user/User.js
@@ -1,8 +1,20 @@
 var mongoose = require('mongoose');
+
+//TODO: add constraints
 var UserSchema = new mongoose.Schema({
   name: String,
   email: String,
-  password: String
+  password: String,
+  phone: String,
+  dob: String,
+  occupation: String,
+  gender: String,
+  interests: [String],
+  organisations: [String],
+  countries: [String],
+  skills: [String],
+  
+  crisisAvailable: Boolean,
 });
 mongoose.model('User', UserSchema);
 
--- backend/user/UserController.js
+++ backend/user/UserController.js
@@ -11,8 +11,17 @@ router.post('/', function (req, res) {
     User.create({
             name : req.body.name,
             email : req.body.email,
-            password : req.body.password
-        }, 
+            password : req.body.password,
+            phone: req.body.phone,
+            dob: req.body.dob,
+            occupation: req.body.occupation,
+            gender: req.body.gender,
+            interests: req.body.interests,
+            organisations: req.body.organisations,
+            countries: req.body.countries,
+            skills: req.body.skills,
+            crisisAvailable: req.body.crisisAvailable
+        },
         function (err, user) {
             if (err) return res.status(500).send("There was a problem adding the information to the database.");
             res.status(200).send(user);
@@ -29,7 +38,7 @@ router.get('/', function (req, res) {
 
 // GETS A SINGLE USER FROM THE DATABASE
 router.get('/:id', function (req, res) {
-    User.findById(req.params.id, function (err, user) {
+    User.findById({_id: req.query.id}, function (err, user) {
         if (err) return res.status(500).send("There was a problem finding the user.");
         if (!user) return res.status(404).send("No user found.");
         res.status(200).send(user);
@@ -38,7 +47,7 @@ router.get('/:id', function (req, res) {
 
 // DELETES A USER FROM THE DATABASE
 router.delete('/:id', function (req, res) {
-    User.findByIdAndRemove(req.params.id, function (err, user) {
+    User.findByIdAndRemove({_id: req.query.id }, function (err, user) {
         if (err) return res.status(500).send("There was a problem deleting the user.");
         res.status(200).send("User: "+ user.name +" was deleted.");
     });
@@ -46,7 +55,7 @@ router.delete('/:id', function (req, res) {
 
 // UPDATES A SINGLE USER IN THE DATABASE
 router.put('/:id', function (req, res) {
-    User.findByIdAndUpdate(req.params.id, req.body, {new: true}, function (err, user) {
+    User.findByIdAndUpdate({_id:req.query.id}, req.body, {new: true}, function (err, user) {
         if (err) return res.status(500).send("There was a problem updating the user.");
         res.status(200).send(user);
     });
