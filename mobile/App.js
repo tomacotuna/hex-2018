d65f78c8f9e3d28467bbb28728d62b46831abec7
Toma-Theodor Cotuna <tomacotuna@gmail.com>
2018-03-17 10:31:45 +0000

    react native bound to api call tested

--- mobile/App.js
+++ mobile/App.js
@@ -5,11 +5,33 @@ import { Button } from "antd-mobile";
 import { Styles } from "./app/decorators/Styles";
 
 export default class App extends React.Component {
+  state = {
+    data: null
+  };
+
+  callAPI = () => {
+    fetch("http://localhost:3000/item", {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      }
+    })
+      .then(response => response.json())
+      .then(responseJson => {
+        console.log(responseJson);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
   render() {
     return (
       <View style={styles.container}>
         <Text>Damnnddsss</Text>
-        <Button type="primary">Butotn</Button>
+        <Button type="primary" onClick={this.callAPI}>
+          Butotn
+        </Button>
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
